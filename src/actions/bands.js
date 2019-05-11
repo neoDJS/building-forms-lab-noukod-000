7536fb7f81b073f513323b82e948b771420ca323
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 04:11:47 +0000

    Automatically backed up by Learn

---
+++ src/actions/bands.js
@@ -0,0 +1,5 @@
+export const addBand = ()=> {
+  return {
+    type: 'ADD_BAND'
+  }
+}
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,39 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state)
+    this.setState({
+      name: ''
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <label>add todo</label>
+          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.name}/>
+          <input type="submit" />
+       </form>
       </div>
     )
   }
 }
 
-export default BandInput
+export default connect(null, { addBands })(BandInput);
